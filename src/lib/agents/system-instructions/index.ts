940c3a9138d989d45f9dcc5a94d0df2acd6b63d6
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-01-17 14:41:30 +0700

    added agents system instructions wrapper

---
+++ src/lib/agents/system-instructions/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Agents System Instruction Wrapper
+ */
+
+export * from "./core";
+export * from "./defined-insight";
+export * from "./extractor";
+export * from "./insight";
+export * from "./products";
+export * from "./related";
