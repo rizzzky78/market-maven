d65283e57b915c26be00fbd42395b5acfe27cd24
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-01-17 19:18:25 +0700

    types for AI agents workflow

---
+++ src/lib/types/ai.ts
@@ -0,0 +1,100 @@
+import { CoreMessage, LanguageModelV1StreamPart } from "ai";
+import { ReactNode } from "react";
+
+export interface ExtendedCoreMessage extends Omit<CoreMessage, "id"> {
+  id: string;
+}
+
+export type MessageProperty = {
+  id: string;
+  role: "user" | "assistant" | "system" | "tool";
+  content: CoreMessage["content"];
+};
+
+export type ChatProperties = {
+  chatId: string;
+  title: string;
+  created: Date;
+  userId: string;
+  messages: MessageProperty[];
+  sharePath?: string;
+};
+
+/**
+ * Construct a set of types that match by given controller
+ * @example For mutating client state generation from server action
+ */
+export type FunctionController = (args?: any) => void | Promise<void>;
+
+export type AssignController = Record<string, FunctionController>;
+
+export type UseAction = {
+  sendMessage: (
+    f: FormData,
+    assignController?: AssignController
+  ) => Promise<SendMessageCallback>;
+};
+
+export type SendMessageCallback = {
+  id: string;
+  display: ReactNode;
+  stream: ReadableStream<LanguageModelV1StreamPart>;
+};
+
+export type AIState = {
+  chatId: string;
+  messages: MessageProperty[];
+  isSharedPage?: boolean;
+};
+
+export type UIState = {
+  id: string;
+  display: ReactNode;
+}[];
+
+export const AvailableTools = {
+  SEARCH_PRODUCT: "searchProduct",
+  GET_PRODUCT_DETAILS: "getProductDetails",
+} as const;
+
+export type AvailableTool =
+  (typeof AvailableTools)[keyof typeof AvailableTools];
+
+export type MutationPayload = {
+  name: AvailableTool;
+  args: unknown;
+  result: unknown;
+  overrideAssistant?: {
+    content: string;
+  };
+};
+
+/**
+ * Construct type from defined tool args and tool result data
+ * @type `A = unknown` is for args
+ * @type `T = unknown` is for data or tool result
+ */
+export type ExtendedToolResult<A = unknown, D = unknown> = {
+  success: boolean;
+  name: string;
+  args: A;
+  data: D;
+};
+
+export type AttachLink = {
+  meta: {
+    id: number | string;
+    title: string;
+    link: string;
+  };
+};
+
+export type ParsedUserMessage = {
+  text_input: string;
+  attach_link?: string;
+};
+
+export type UserMessageProp = {
+  text_input: string;
+  attach_link?: string;
+};
