88cc4809c83107bb143151bba44fa6989b9a67ab
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-01-19 21:49:37 +0700

    component UI: added app-sidebar, this behave as sidebar menu content

---
+++ src/components/maven/app-sidebar.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { BaggageClaim, CirclePlus } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Session } from "next-auth";
+import { ChatProperties } from "@/lib/types/ai";
+import { useAIState, useUIState } from "ai/rsc";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import { useAppState } from "@/lib/utility/provider/app-state-provider";
+import { AI } from "@/app/action";
+import { useSmartTextarea } from "../hooks/use-smart-textare";
+import { Button } from "../ui/button";
+
+interface AppSidebarProps extends React.ComponentProps<typeof Sidebar> {
+  session: Session | null;
+  chats: ChatProperties[];
+}
+
+export function AppSidebar({ chats, session, ...props }: AppSidebarProps) {
+  const user = {
+    name: session?.user?.name as string,
+    email: session?.user?.email as string,
+    avatar: session?.user?.image as string,
+  };
+
+  const { isGenerating, setIsGenerating } = useAppState();
+  const { flush } = useSmartTextarea();
+  const [_ui, setUIState] = useUIState<typeof AI>();
+  const [_ai, setAIState] = useAIState<typeof AI>();
+
+  const router = useRouter();
+
+  const handleNewChat = () => {
+    setIsGenerating(false);
+    flush();
+    setUIState([]);
+    setAIState({ chatId: "", messages: [] });
+    router.push("/chat");
+  };
+
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center pb-1 justify-between">
+            <div className="flex items-center space-x-2">
+              <BaggageClaim className="text-purple-400" />
+              <span className="font-bold">MarketMaven</span>
+            </div>
+            <motion.div whileHover={{ rotate: 180, scale: 1.3 }}>
+              <Button
+                size={"icon"}
+                variant={"ghost"}
+                className="rounded-full"
+                onClick={handleNewChat}
+                disabled={isGenerating}
+              >
+                <CirclePlus />
+                <span className="sr-only">New Chat</span>
+              </Button>
+            </motion.div>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent className="pr-2 scrollbar-thin">
+        {chats.map((chat, idx) => (
+          <HistoryItem key={idx} chat={chat} />
+        ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
