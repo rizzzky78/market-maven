e2d268b081485a2846d9b7196df11f203d3e8a26
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-01-19 22:39:20 +0700

    add route page for /chat/c/ as act for default query search page

---
+++ src/app/chat/c/page.tsx
@@ -0,0 +1,36 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { cache, FC } from "react";
+import { getChats } from "@/lib/agents/action/chat-service";
+import { v4 } from "uuid";
+import { AI } from "@/app/action";
+import { Chat } from "@/components/maven/chat";
+
+export const maxDuration = 60;
+
+interface PageProps {
+  searchParams: Promise<{ q: string }>;
+}
+
+const loadChats = cache(async (userId: string) => {
+  return await getChats(userId);
+});
+
+const Page: FC<PageProps> = async ({ searchParams }) => {
+  const { q } = await searchParams;
+  if (!q) {
+    redirect("/chat");
+  }
+  const id = v4();
+  const session = await getServerSession();
+
+  const chats = await loadChats(session?.user?.email || "anonymous");
+
+  return (
+    <AI initialAIState={{ chatId: id, messages: [] }}>
+      <Chat id={id} query={searchParams.q} chats={chats} />
+    </AI>
+  );
+};
+
+export default Page;
