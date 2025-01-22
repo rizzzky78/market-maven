b3b6ce97c2b08489d2d8738ea90f0476e101d3f5
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-01-22 23:12:04 +0700

    modify history-item props as now accept stream-generation state, it disabled when in generating-state

--- src/components/maven/history-item.tsx
+++ src/components/maven/history-item.tsx
@@ -2,22 +2,30 @@ import { ChatProperties } from "@/lib/types/ai";
 import Link from "next/link";
 import { FC } from "react";
 import { useSetChatTitle } from "../hooks/use-set-chat-title";
+import { Button } from "../ui/button";
 
 interface HistoryItemProps {
   chat: ChatProperties;
+  disabled?: boolean;
 }
 
-export const HistoryItem: FC<HistoryItemProps> = ({ chat }) => {
+export const HistoryItem: FC<HistoryItemProps> = ({ chat, disabled }) => {
   const { setChatTitle } = useSetChatTitle();
   return (
-    <Link
-      href={`/chat/c/${chat.chatId}`}
-      className="flex whitespace-nowrap px-2 py-1 text-sm leading-relaxed last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground rounded-sm"
-      onClick={() => setChatTitle(chat.title)}
+    <Button
+      variant={"ghost"}
+      className="font-normal px-0 text-start last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground rounded-sm"
+      disabled={disabled}
     >
-      <span className="line-clamp-2 whitespace-break-spaces text-xs">
-        {chat.title}
-      </span>
-    </Link>
+      <Link
+        href={`/chat/c/${chat.chatId}`}
+        className="flex whitespace-nowrap px-2 py-1 text-sm leading-relaxed"
+        onClick={() => setChatTitle(chat.title)}
+      >
+        <span className="line-clamp-2 whitespace-break-spaces text-xs">
+          {chat.title}
+        </span>
+      </Link>
+    </Button>
   );
 };
