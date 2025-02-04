0663d1f26c13004c4037c0f4c77fec90c86a9537
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-02-04 22:16:03 +0700

    feat: added func unified-user-payload-message

--- src/lib/agents/action/mutator/mutate-messages.ts
+++ src/lib/agents/action/mutator/mutate-messages.ts
@@ -1,4 +1,8 @@
-import { MessageProperty } from "@/lib/types/ai";
+import {
+  MessageProperty,
+  PayloadData,
+  UserContentMessage,
+} from "@/lib/types/ai";
 import { CoreMessage } from "ai";
 
 /**
@@ -9,4 +13,21 @@ import { CoreMessage } from "ai";
  */
 export const toCoreMessage = (m: MessageProperty[]): CoreMessage[] => {
   return m.map((m) => ({ role: m.role, content: m.content } as CoreMessage));
+};
+
+/**
+ * Converts payload object data to unified user content message
+ *
+ * @param payload - A payload of optional properties
+ * @returns An object contain at least one property of UserContentMessage
+ */
+export const toUnifiedUserMessage = (payload: PayloadData) => {
+  const { textInput, attachProduct, productCompare, inquiryResponse } = payload;
+
+  return {
+    text_input: textInput ?? null,
+    attach_product: attachProduct ?? null,
+    product_compare: productCompare ?? null,
+    inquiry_response: inquiryResponse ?? null,
+  } as UserContentMessage;
 };
