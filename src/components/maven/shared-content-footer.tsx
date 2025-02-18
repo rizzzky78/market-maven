302b1eba1b36988757f096995d2fe4cdc4fe7122
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-02-18 18:56:33 +0700

    feat: adjust legacy prop on footer background image

--- src/components/maven/shared-content-footer.tsx
+++ src/components/maven/shared-content-footer.tsx
@@ -16,9 +16,8 @@ export const ContentSharedFooter: FC = () => {
           <Image
             src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a31965e5-2811-4f62-96c0-f7fde5f222a5/de5q1i0-f76b868f-056f-4896-af94-85cfa22152aa.jpg/v1/fill/w_1192,h_670,q_70,strp/element_of_life_by_kuzy62_de5q1i0-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvYTMxOTY1ZTUtMjgxMS00ZjYyLTk2YzAtZjdmZGU1ZjIyMmE1XC9kZTVxMWkwLWY3NmI4NjhmLTA1NmYtNDg5Ni1hZjk0LTg1Y2ZhMjIxNTJhYS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.kgirUYc4Qs6hcQfMCCZRR9EvMIVnICihEMv5O_c_6Jg"
             alt="Footer background"
-            layout="fill"
-            objectFit="cover"
-            className="opacity-40 rounded-t-[2rem]"
+            fill
+            className="opacity-40 object-cover rounded-t-[2rem]"
           />
         </div>
         <div className="container mx-auto px-4 relative z-10">
