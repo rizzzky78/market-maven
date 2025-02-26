8011209eb94f44cd177f8a7089c105bbea57f508
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-02-26 14:30:47 +0700

    feat: modify quick action panel, change template query

--- src/components/maven/quick-action.tsx
+++ src/components/maven/quick-action.tsx
@@ -2,11 +2,11 @@
 "use client";
 
 import {
-  BookHeadphones,
-  CircleHelp,
+  Gamepad,
   Headphones,
   Laptop,
   Laptop2,
+  RectangleHorizontal,
   Smartphone,
 } from "lucide-react";
 import { FC, ReactNode, useCallback } from "react";
@@ -21,41 +21,32 @@ import { useSmartTextarea } from "../hooks/use-smart-textare";
 import { toast } from "sonner";
 
 const predefinedActions: {
-  label: string;
   action: string;
   icon: ReactNode;
 }[] = [
   {
-    label: "Search for dBE Headphone",
-    action: "Search for dBE Headphone",
-    icon: <Headphones className="size-4 shrink-0 mr-2" />,
+    action: "Search for Samsung Galaxy S24 Ultra",
+    icon: <Smartphone className="size-5 mr-2 shrink-0" />,
   },
   {
-    label:
-      "What is good from this product https://www.tokopedia.com/soundcorebyanker/new-launch-true-wireless-earbuds-anker-soundcore-a20i-a3948-black-ecda3",
-    action:
-      "What is good from this product https://www.tokopedia.com/soundcorebyanker/new-launch-true-wireless-earbuds-anker-soundcore-a20i-a3948-black-ecda3",
-    icon: <BookHeadphones className="size-4 shrink-0 mr-2" />,
+    action: "I want to buy a new laptop with gaming specs",
+    icon: <Laptop2 className="size-5 mr-2 shrink-0" />,
   },
   {
-    label: "Comparison between Lenovo Yoga and Legion for gaming purpose",
-    action: "Comparison between Lenovo Yoga and Legion for gaming purpose",
-    icon: <Laptop className="size-4 shrink-0 mr-2" />,
+    action: "Search for Lenovo Yoga and check if it matches my preferences.",
+    icon: <Laptop className="size-5 mr-2 shrink-0" />,
   },
   {
-    label: "What are the important key points in buying a product?",
-    action: "What are the important key points in buying a product?",
-    icon: <CircleHelp className="size-4 shrink-0 mr-2" />,
+    action: "I want buy handheld console, do you have suggestions?",
+    icon: <Gamepad className="size-5 mr-2 shrink-0" />,
   },
   {
-    label: "A smartphone with good specifications at an affordable price",
-    action: "A smartphone with good specifications at an affordable price",
-    icon: <Smartphone className="size-4 shrink-0 mr-2" />,
+    action: "Im looking for discrete graphic for my PC",
+    icon: <RectangleHorizontal className="size-5 mr-2 shrink-0" />,
   },
   {
-    label: "Search for Lenovo Yoga",
-    action: "Search for Lenovo Yoga",
-    icon: <Laptop2 className="size-4 shrink-0 mr-2" />,
+    action: "Search for dBE headphone",
+    icon: <Headphones className="size-5 mr-2 shrink-0" />,
   },
 ];
 
@@ -137,33 +128,99 @@ export const QuickActionButton: FC = () => {
   );
   return (
     <div className="w-full">
-      <div className="flex justify-center mb-2">
-        <div className="size-36">
-          <svg className="clipppy absolute -top-[999px] -left-[999px] w-0 h-0">
-            <defs>
-              <clipPath id="clip-goey6" clipPathUnits={"objectBoundingBox"}>
+      <div className="mb-20">
+        <div className="flex justify-center">
+          <svg
+            className="coolshapes triangle-6 "
+            height="200"
+            width="200"
+            fill="none"
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#cs_clip_1_triangle-6)">
+              <mask
+                height="200"
+                id="cs_mask_1_triangle-6"
+                style={{ maskType: "alpha" }}
+                width="200"
+                x="0"
+                y="0"
+                maskUnits="userSpaceOnUse"
+              >
                 <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1 0H0V0.5L3.31881e-08 1H0.489388C0.771391 1 1 0.776142 1 0.5H0.489388C0.771391 0.5 1 0.276142 1 0Z"
-                  fill=""
+                  d="M200 0v200L0 0h200zM100 100v100L0 100h100z"
+                  fill="#fff"
                 />
+              </mask>
+              <g mask="url(#cs_mask_1_triangle-6)">
+                <path d="M200 0H0v200h200V0z" fill="#fff" />
+                <path d="M200 0H0v200h200V0z" fill="#0E6FFF" />
+                <g filter="url(#filter0_f_748_4934)">
+                  <path
+                    d="M243.892 107.197h-222v116h222v-116z"
+                    fill="#8F5BFF"
+                  />
+                  <ellipse
+                    cx="68.935"
+                    cy="-27.395"
+                    fill="#00F0FF"
+                    rx="111.935"
+                    ry="63.605"
+                  />
+                </g>
+              </g>
+            </g>
+            <g
+              style={{ mixBlendMode: "overlay" }}
+              mask="url(#cs_mask_1_triangle-6)"
+            >
+              <path
+                d="M200 0H0v200h200V0z"
+                fill="gray"
+                stroke="transparent"
+                filter="url(#cs_noise_1_triangle-6)"
+              />
+            </g>
+            <defs>
+              <filter
+                height="434.197"
+                id="filter0_f_748_4934"
+                width="406.892"
+                x="-103"
+                y="-151"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+              >
+                <feFlood result="BackgroundImageFix" floodOpacity="0" />
+                <feBlend
+                  result="shape"
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                />
+                <feGaussianBlur
+                  result="effect1_foregroundBlur_748_4934"
+                  stdDeviation="30"
+                />
+              </filter>
+              <clipPath id="cs_clip_1_triangle-6">
+                <path d="M0 0H200V200H0z" fill="#fff" />
               </clipPath>
             </defs>
+            <defs>
+              <filter
+                height="100%"
+                id="cs_noise_1_triangle-6"
+                width="100%"
+                x="0%"
+                y="0%"
+                filterUnits="objectBoundingBox"
+              >
+                <feBlend result="out3" in="SourceGraphic" in2="out2" />
+              </filter>
+            </defs>
           </svg>
-          <figure className="rounded-xl">
-            <div style={{ clipPath: "url(#clip-goey6)" }}>
-              <img
-                className="align-bottom aspect-square object-cover w-full"
-                src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8ce83fbc-32a6-4f2f-b617-b1a4c6719ef8/dg9xdq7-cf97a633-b3de-40c4-be6a-6829ececec8d.jpg/v1/fit/w_828,h_1076,q_70,strp/molded_abstract_by_bwall49_dg9xdq7-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTY2NCIsInBhdGgiOiJcL2ZcLzhjZTgzZmJjLTMyYTYtNGYyZi1iNjE3LWIxYTRjNjcxOWVmOFwvZGc5eGRxNy1jZjk3YTYzMy1iM2RlLTQwYzQtYmU2YS02ODI5ZWNlY2VjOGQuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.IV5r2d0QuFQZrSoZfmDvDD1jPa_CidRAhj0RkxArnPU"
-                alt=""
-              />
-            </div>
-          </figure>
         </div>
-      </div>
-      <div className="mb-10 font-bold flex items-center justify-center">
-        <h2>MarketMaven</h2>
       </div>
       <div className="mb-2 mt-2 grid gap-2 grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
         {predefinedActions.map((actionItem, index) => (
@@ -173,10 +230,12 @@ export const QuickActionButton: FC = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3, delay: index * 0.1 }}
-            className="text-xs cursor-pointer hover:text-black hover:bg-purple-200 flex items-center bg-muted h-14 rounded-3xl px-5 py-3 w-full"
+            className="text-xs cursor-pointer hover:text-black hover:bg-purple-200 flex items-center bg-muted h-12 rounded-3xl px-5 py-2 w-full"
           >
             {actionItem.icon}
-            <p className="text-xs text-left line-clamp-2">{actionItem.label}</p>
+            <p className="text-xs text-left line-clamp-2">
+              {actionItem.action}
+            </p>
           </motion.button>
         ))}
       </div>
