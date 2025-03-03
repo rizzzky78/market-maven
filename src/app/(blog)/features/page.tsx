5051b255b660e54ecbfb17f87bf83320fc994e06
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-03-03 23:41:02 +0700

    feat: reafctor, adjust aligment of content page, modularize page comonent

--- src/app/(blog)/features/page.tsx
+++ src/app/(blog)/features/page.tsx
@@ -1,13 +1,13 @@
 import { Features } from "@/components/maven/features";
+import { Footer } from "@/components/maven/footer";
 import { NavigationBar } from "@/components/maven/navigation-bar";
 
 export default function Page() {
   return (
     <div>
       <NavigationBar />
-      <div className="w-full flex justify-center">
-        <Features />
-      </div>
+      <Features />
+      <Footer />
     </div>
   );
 }
