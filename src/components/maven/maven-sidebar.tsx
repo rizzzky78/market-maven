8f4ecf8a3aaa1d24c662e3cc7ba613a9e1018cfd
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-03-04 17:28:10 +0700

    feat: modify icon with svg instead

--- src/components/maven/maven-sidebar.tsx
+++ src/components/maven/maven-sidebar.tsx
@@ -3,6 +3,7 @@
 import { ComponentProps, FC, useEffect, useState } from "react";
 import {
   CodeXml,
+  Hexagon,
   History,
   MessageCircleQuestion,
   Moon,
@@ -99,11 +100,104 @@ export const MavenSidebar: FC<MavenSidebarProps> = ({
                 className="rounded-full md:h-8 md:p-0"
               >
                 <Link href="/">
-                  <div className="flex aspect-square size-8 items-center justify-center">
-                    <MessageCircleQuestion className="size-4 text-purple-400" />
+                  <div className="flex aspect-square size-7 items-center justify-center">
+                    <svg
+                      className="coolshapes polygon-7 "
+                      height="100"
+                      width="100"
+                      fill="none"
+                      viewBox="0 0 200 200"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#cs_clip_1_polygon-7)">
+                        <mask
+                          height="200"
+                          id="cs_mask_1_polygon-7"
+                          style={{ maskType: "alpha" }}
+                          width="182"
+                          x="9"
+                          y="0"
+                          maskUnits="userSpaceOnUse"
+                        >
+                          <path
+                            d="M86.449 3.601a27.296 27.296 0 0127.102 0l63.805 36.514C185.796 44.945 191 53.9 191 63.594v72.812c0 9.694-5.204 18.649-13.644 23.479l-63.805 36.514a27.3 27.3 0 01-27.102 0l-63.805-36.514C14.204 155.055 9 146.1 9 136.406V63.594c0-9.694 5.204-18.649 13.644-23.48L86.45 3.602z"
+                            fill="#fff"
+                          />
+                        </mask>
+                        <g mask="url(#cs_mask_1_polygon-7)">
+                          <path d="M200 0H0v200h200V0z" fill="#fff" />
+                          <path d="M200 0H0v200h200V0z" fill="#0E6FFF" />
+                          <g filter="url(#filter0_f_748_4355)">
+                            <path d="M209 126H-9v108h218V126z" fill="#8F5BFF" />
+                            <ellipse
+                              cx="87"
+                              cy="57.5"
+                              fill="#00F0FF"
+                              rx="59"
+                              ry="34.5"
+                            />
+                          </g>
+                        </g>
+                      </g>
+                      <g
+                        style={{ mixBlendMode: "overlay" }}
+                        mask="url(#cs_mask_1_polygon-7)"
+                      >
+                        <path
+                          d="M200 0H0v200h200V0z"
+                          fill="gray"
+                          stroke="transparent"
+                          filter="url(#cs_noise_1_polygon-7)"
+                        />
+                      </g>
+                      <defs>
+                        <filter
+                          height="331"
+                          id="filter0_f_748_4355"
+                          width="338"
+                          x="-69"
+                          y="-37"
+                          filterUnits="userSpaceOnUse"
+                          colorInterpolationFilters="sRGB"
+                        >
+                          <feFlood
+                            result="BackgroundImageFix"
+                            floodOpacity="0"
+                          />
+                          <feBlend
+                            result="shape"
+                            in="SourceGraphic"
+                            in2="BackgroundImageFix"
+                          />
+                          <feGaussianBlur
+                            result="effect1_foregroundBlur_748_4355"
+                            stdDeviation="30"
+                          />
+                        </filter>
+                        <clipPath id="cs_clip_1_polygon-7">
+                          <path d="M0 0H200V200H0z" fill="#fff" />
+                        </clipPath>
+                      </defs>
+                      <defs>
+                        <filter
+                          height="100%"
+                          id="cs_noise_1_polygon-7"
+                          width="100%"
+                          x="0%"
+                          y="0%"
+                          filterUnits="objectBoundingBox"
+                        >
+                          <feBlend
+                            result="out3"
+                            in="SourceGraphic"
+                            in2="out2"
+                          />
+                        </filter>
+                      </defs>
+                    </svg>
                   </div>
                   <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-semibold">Market Maven</span>
+                    <span className="truncate font-semibold">Maven</span>
                     <span className="truncate text-xs">
                       AI & Data Driven purchase decision
                     </span>
@@ -267,8 +361,8 @@ export const MavenSidebar: FC<MavenSidebarProps> = ({
         <SidebarHeader className="gap-3.5 border-b px-4 py-3">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center text-base font-medium text-foreground">
-              <MessageCircleQuestion className="size-5 shrink-0 mr-2" />
-              <p>Market Maven</p>
+              <Hexagon className="size-5 shrink-0 mr-2 text-purple-500" />
+              <p>Maven</p>
             </div>
           </div>
         </SidebarHeader>
