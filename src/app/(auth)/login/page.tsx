e32f988a6ba4fb0f3543eada25181b13fb41b366
rizzzky78 <rizzzky78@users.noreply.github.com>
2025-03-09 15:23:11 +0700

    feat: added on top level server component auth login verification

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,12 @@
 import { LoginForm } from "@/components/maven/login-form";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession();
+
+  if (session) redirect("/chat");
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-7xl">
